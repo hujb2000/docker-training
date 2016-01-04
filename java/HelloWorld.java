5d429bf9ebad841bc20ca7f5bee11eb1e9990103
胡家宝 <jiabao_hu@corp.netease.com>
2016-01-04 19:00:49 +0800

    java HelloWorld

---
+++ java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        hello();
+    }
+    
+    private static void hello(){
+        System.out.println("Hello World!");
+    }
+}
